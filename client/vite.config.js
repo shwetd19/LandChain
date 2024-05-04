432dc67566fa4d70c3cd83a3fa4e6a15c206be8f
shwetd19 <shwetas.dhake@mitaoe.ac.in>
2024-05-05 00:21:40 +0530

    Backend Production Error Fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        // target: 'http://localhost:3000',
+        target: "https://land-chain-eta.vercel.app/",
         secure: false,
       },
     },
